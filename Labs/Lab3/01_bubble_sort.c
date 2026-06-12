dcc8eb64c899f4ae62438f7f4ab452a817e865b0
Bhavy Kakaniya <bhavy2kakaniya@gmail.com>
2026-06-12 16:10:41 +0530

    solved

--- Labs/Lab3/01_bubble_sort.c
+++ Labs/Lab3/01_bubble_sort.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-int bubble_sort(int length, int arr[])
+void bubble_sort(int length, int arr[])
 {
-    for(int i = 0; i < length - 1; i++) {
-        for(int j = 0; j < length - 1; j++){
-            
+    for (int i = 0; i < length - 1; i++)
+    {
+        bool swapped = false;
+        for (int j = 0; j < length - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swapped = true;
+                arr[j] = arr[j] + arr[j + 1];
+                arr[j + 1] = arr[j] - arr[j + 1];
+                arr[j] = arr[j] - arr[j + 1];
+            }
         }
+        if(!swapped) {
+            break;
+        }
+    }
+}
+
+void print_arr(int length, int arr[])
+{
+    for (int i = 0; i < length; i++)
+    {
+        printf("%d ", arr[i]);
     }
 }
 
@@ -13,4 +34,6 @@ int main()
 {
     int arr[] = {4, 5, 7, 6, 10, 9, 8, 1, 2, 3};
     int length = sizeof(arr) / sizeof(arr[0]);
+    bubble_sort(length, arr);
+    print_arr(length, arr);
 }
