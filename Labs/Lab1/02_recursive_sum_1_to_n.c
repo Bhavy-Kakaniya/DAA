754b75be0d0441bcf5a7d25ff207be6ce2c83fae
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2026-06-12 10:38:14 +0530

    sum upto n using loop and recursion and calculating time

--- Labs/Lab1/02_recursive_sum_1_to_n.c
+++ Labs/Lab1/02_recursive_sum_1_to_n.c
@@ -1,14 +1,44 @@
 #include <stdio.h>
+#include <time.h>
 
-int sum(int n) {
-    if(n == 1) {
+int sum_recursive(int n)
+{
+    if (n == 1)
+    {
         return 1;
     }
-    return sum(n - 1) + n;
+    return sum_recursive(n - 1) + n;
+}
+
+int sum_iterative(int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    return sum;
 }
 
 void main()
 {
-    int n = 8;
-    printf("%d", sum(n));
+    int n;
+    printf("Enter number : ");
+    scanf("%d", &n);
+
+    clock_t start, end;
+    double time_taken;
+
+    start = clock();
+    printf("\n%d using iteration", sum_iterative(n));
+    end = clock();
+    time_taken = ((double)(end - start));
+    printf("\ntime taken using iterative function : %f", time_taken);
+    
+    start = clock();
+    printf("\n%d using recursion", sum_iterative(n));
+    end = clock();
+    time_taken = ((double)(end - start));
+    printf("\ntime taken using iterative function : %f", time_taken);
+
 }
