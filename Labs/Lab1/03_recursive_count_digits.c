3d3d7ed5a9891741a929b97fba55d345f1144057
Bhavy-Kakaniya <bhavy2kakaniya@gmail.com>
2026-06-12 10:39:24 +0530

    count digit using recursion and iteration

--- Labs/Lab1/03_recursive_count_digits.c
+++ Labs/Lab1/03_recursive_count_digits.c
@@ -1,17 +1,47 @@
 #include <stdio.h>
+#include <time.h>
 
-int count = 0;
-
-int digit(int n) {
-    if(n == 0) {
+int digit_recursive(int n, int count)
+{
+    if (n == 0)
+    {
         return count;
     }
     count++;
-    return digit(n / 10);
+    return digit_recursive(n / 10, count);
+}
+
+int digit_iterative(int n)
+{
+    int count = 0;
+    while (n != 0)
+    {
+        n /= 10;
+        count++;
+    }
+    return count;
 }
 
 void main()
 {
-    int n = 1234567;
-    printf("%d", digit(n));
+    int n;
+    int count = 0;
+
+    printf("Enter number : ");
+    scanf("%d", &n);
+
+    clock_t start, end;
+    double time_taken;
+
+    start = clock();
+    printf("\n%d using iteration", digit_iterative(n));
+    end = clock();
+    time_taken = ((double)(end - start));
+    printf("\ntime taken using iterative function : %f", time_taken);
+
+    start = clock();
+    printf("\n%d using recursion", digit_recursive(n, count));
+    end = clock();
+    time_taken = ((double)(end - start));
+    printf("\ntime taken using recursion function : %f", time_taken);
 }
