1b3388f255e92b9b8da0569c037d045c5f697111
Bhavy Kakaniya <bhavy2kakaniya@gmail.com>
2026-06-30 00:02:57 +0530

    optimizing code for helper functions

--- Labs/Lab3/01.c
+++ Labs/Lab3/01.c
@@ -2,11 +2,11 @@
 #include <time.h>
 #include <stdlib.h>
 
-double bubble_sort(int arr[], int n)
+void bubble_sort(int arr[], int n)
 {
     int i, j, temp;
-    clock_t start, end;
-    start = clock();
+    // clock_t start, end;
+    // start = clock();
     for (i = 0; i < n - 1; i++)
     {
         for (j = 0; j < n - i - 1; j++)
@@ -19,102 +19,137 @@ double bubble_sort(int arr[], int n)
             }
         }
     }
+    // end = clock();
+    // return (double)(end - start) / CLOCKS_PER_SEC;
+}
+
+// void bubble_sort_best_case_1000(int n)
+// {
+//     int arr[n];
+//     FILE *fp_bestcase_1000;
+
+//     fp_bestcase_1000 = fopen("bestcase.txt", "w");
+//     for (int i = 0; i < n; i++)
+//     {
+//         fprintf(fp_bestcase_1000, "%d ", i);
+//     }
+//     fclose(fp_bestcase_1000);
+//     fp_bestcase_1000 = fopen("bestcase.txt", "r");
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         fscanf(fp_bestcase_1000, "%d", &arr[i]);
+//     }
+
+//     fclose(fp_bestcase_1000);
+//     printf("\nbest case, size 1000: %f", bubble_sort(arr, n));
+// }
+
+// void bubble_sort_worst_case_1000(int n)
+// {
+//     int arr[n];
+//     FILE *fp_worstcase_1000;
+
+//     fp_worstcase_1000 = fopen("worstcase.txt", "w");
+//     for (int i = 0; i < n; i++)
+//     {
+//         fprintf(fp_worstcase_1000, "%d ", i);
+//     }
+//     fclose(fp_worstcase_1000);
+//     fp_worstcase_1000 = fopen("worstcase.txt", "r");
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         fscanf(fp_worstcase_1000, "%d", &arr[i]);
+//     }
+
+//     fclose(fp_worstcase_1000);
+//     printf("\nworst case, size 1000: %f", bubble_sort(arr, n));
+// }
+
+// void bubble_sort_average_case_1000(int n)
+// {
+//     int arr[n];
+//     FILE *fp_averagecase_1000;
+
+//     fp_averagecase_1000 = fopen("averagecase.txt", "w");
+//     for (int i = 0; i < n; i++)
+//     {
+//         fprintf(fp_averagecase_1000, "%d ", rand() % n);
+//     }
+//     fclose(fp_averagecase_1000);
+//     fp_averagecase_1000 = fopen("averagecase.txt", "r");
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         fscanf(fp_averagecase_1000, "%d", &arr[i]);
+//     }
+
+//     fclose(fp_averagecase_1000);
+//     printf("\naverage case, size 1000: %f", bubble_sort(arr, n));
+// }
+
+// void bubble_sort_average_case_10000(int n)
+// {
+//     int arr[n];
+//     FILE *fp_averagecase_10000;
+
+//     fp_averagecase_10000 = fopen("averagecase10000.txt", "w");
+//     for (int i = 0; i < n; i++)
+//     {
+//         fprintf(fp_averagecase_10000, "%d ", rand() % n);
+//     }
+//     fclose(fp_averagecase_10000);
+//     fp_averagecase_10000 = fopen("averagecase10000.txt", "r");
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         fscanf(fp_averagecase_10000, "%d", &arr[i]);
+//     }
+
+//     fclose(fp_averagecase_10000);
+//     printf("\naverage case, size 10000: %f", bubble_sort(arr, n));
+// }
+
+void f(int n, FILE *fp, char filename[])
+{
+    fp = fopen(filename, "w");
+    for (int i = 0; i < n; i++)
+    {
+        fprintf(fp, "%d ", i);
+    }
+    fclose(fp);
+}
+
+double calculateTime_F(char filename[], int n)
+{
+    int arr[n];
+    FILE *fp = fopen(filename, "r");
+    int i;
+
+    for (i = 0; i < n; i++)
+        fscanf(fp, "%d", &arr[i]);
+
+    fclose(fp);
+
+    clock_t start, end;
+
+    start = clock();
+    bubble_sort(arr, n);
     end = clock();
+
     return (double)(end - start) / CLOCKS_PER_SEC;
-}
-
-void bubble_sort_best_case_1000(int n)
-{
-    int arr[n];
-    FILE *fp_bestcase_1000;
-
-    fp_bestcase_1000 = fopen("bestcase.txt", "w");
-    for (int i = 0; i < n; i++)
-    {
-        fprintf(fp_bestcase_1000, "%d ", i);
-    }
-    fclose(fp_bestcase_1000);
-    fp_bestcase_1000 = fopen("bestcase.txt", "r");
-
-    for (int i = 0; i < n; i++)
-    {
-        fscanf(fp_bestcase_1000, "%d", &arr[i]);
-    }
-
-    fclose(fp_bestcase_1000);
-    printf("\nbest case, size 1000: %f", bubble_sort(arr, n));
-}
-
-void bubble_sort_worst_case_1000(int n)
-{
-    int arr[n];
-    FILE *fp_worstcase_1000;
-
-    fp_worstcase_1000 = fopen("worstcase.txt", "w");
-    for (int i = 0; i < n; i++)
-    {
-        fprintf(fp_worstcase_1000, "%d ", i);
-    }
-    fclose(fp_worstcase_1000);
-    fp_worstcase_1000 = fopen("worstcase.txt", "r");
-
-    for (int i = 0; i < n; i++)
-    {
-        fscanf(fp_worstcase_1000, "%d", &arr[i]);
-    }
-
-    fclose(fp_worstcase_1000);
-    printf("\nworst case, size 1000: %f", bubble_sort(arr, n));
-}
-
-void bubble_sort_average_case_1000(int n)
-{
-    int arr[n];
-    FILE *fp_averagecase_1000;
-
-    fp_averagecase_1000 = fopen("averagecase.txt", "w");
-    for (int i = 0; i < n; i++)
-    {
-        fprintf(fp_averagecase_1000, "%d ", rand() % n);
-    }
-    fclose(fp_averagecase_1000);
-    fp_averagecase_1000 = fopen("averagecase.txt", "r");
-
-    for (int i = 0; i < n; i++)
-    {
-        fscanf(fp_averagecase_1000, "%d", &arr[i]);
-    }
-
-    fclose(fp_averagecase_1000);
-    printf("\naverage case, size 1000: %f", bubble_sort(arr, n));
-}
-
-void bubble_sort_average_case_10000(int n)
-{
-    int arr[n];
-    FILE *fp_averagecase_10000;
-
-    fp_averagecase_10000 = fopen("averagecase10000.txt", "w");
-    for (int i = 0; i < n; i++)
-    {
-        fprintf(fp_averagecase_10000, "%d ", rand() % n);
-    }
-    fclose(fp_averagecase_10000);
-    fp_averagecase_10000 = fopen("averagecase10000.txt", "r");
-
-    for (int i = 0; i < n; i++)
-    {
-        fscanf(fp_averagecase_10000, "%d", &arr[i]);
-    }
-
-    fclose(fp_averagecase_10000);
-    printf("\naverage case, size 10000: %f", bubble_sort(arr, n));
 }
 
 void main()
 {
-    bubble_sort_best_case_1000(1000);
-    bubble_sort_worst_case_1000(1000);
-    bubble_sort_average_case_1000(1000);
-    bubble_sort_average_case_10000(100000);
+    // bubble_sort_best_case_1000(1000);
+    // bubble_sort_worst_case_1000(1000);
+    // bubble_sort_average_case_1000(1000);
+    // bubble_sort_average_case_10000(100000);
+
+    int size = 100000;
+    FILE *fp10;
+    f(size, fp10, "abc.txt");
+    printf("%f, ", calculateTime_F("abc.txt", size));
 }
